8ef0e9bc91a3d1d2b37528e6202f4984991ef781
koutch <>
2008-06-22 15:36:17 +0000

    Correct format

--- sf.eclipse.javacc/src-plugin/sf/eclipse/javacc/filters/FilterGeneratedJavaCCFiles.java
+++ sf.eclipse.javacc/src-plugin/sf/eclipse/javacc/filters/FilterGeneratedJavaCCFiles.java
@@ -5,27 +5,20 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
-
 import sf.eclipse.javacc.IJJConstants;
 
-public class FilterGeneratedJavaCCFiles extends ViewerFilter implements IJJConstants
-{
-	public boolean select(Viewer viewer, Object parentElement, Object obj)
-	{
-		if (obj instanceof IAdaptable)
-		{
-			IResource resource = (IResource) ((IAdaptable) obj).getAdapter(IResource.class);
-			if (resource != null)
-			{
-				try
-				{
-					return !(resource.isDerived() && resource.getPersistentProperty(QN_GENERATED_FILE) != null);
-				}
-				catch (CoreException e)
-				{
-				}
-			}
-		}
-		return true;
-	}
+public class FilterGeneratedJavaCCFiles extends ViewerFilter implements IJJConstants {
+  public boolean select(Viewer viewer, Object parentElement, Object obj) {
+    if (obj instanceof IAdaptable) {
+      IResource resource = (IResource) ((IAdaptable) obj).getAdapter(IResource.class);
+      if (resource != null) {
+        try {
+          return !(resource.isDerived() && resource
+              .getPersistentProperty(QN_GENERATED_FILE) != null);
+        }
+        catch (CoreException e) {}
+      }
+    }
+    return true;
+  }
 }
