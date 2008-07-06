8f05d7e5cc4a0f9d5cb1a36ade975d3391e0193d
koutch <>
2008-07-06 16:48:18 +0000

    Add JAVACODE for folding

---
+++ sf.eclipse.javacc/src-plugin/sf/eclipse/javacc/parser/ASTjavacode_production.java
@@ -0,0 +1,11 @@
+package sf.eclipse.javacc.parser;
+
+public class ASTjavacode_production extends JJNode{
+  public ASTjavacode_production(int id) {
+    super(id);
+  }
+
+  public ASTjavacode_production(JavaCCParser p, int id) {
+    super(p, id);
+  }
+}
